2981dd81138aedf610b57a900972f3c11befdb81
ChetuXd <114259982+ChetuXd@users.noreply.github.com>
2023-07-04 18:58:58 +0530

    hello world

--- src/javaCode/HelloWorld.java
+++ src/javaCode/HelloWorld.java
@@ -2,6 +2,6 @@ package javaCode;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
