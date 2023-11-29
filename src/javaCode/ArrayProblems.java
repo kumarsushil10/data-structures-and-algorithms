89f0af727b43fbe7bb262fa5653e20697aecfc43
Sushil Kumar <kmrsushil10@gmail.com>
2023-11-29 10:21:20 +0530

    array element rotation

--- src/javaCode/ArrayProblems.java
+++ src/javaCode/ArrayProblems.java
@@ -28,4 +28,27 @@ public class ArrayProblems {
             System.out.println(" " + key + "  ====  " + frequency.get(key));
         }
     }
+
+    public void leftRotation(int[] nums, int rotation) {
+        for (int i = 0; i < rotation; i++) {
+            int j, first = nums[0];
+            for (j = 0; j < nums.length - 1; j++) {
+                nums[j] = nums[j + 1];
+            }
+            nums[j] = first;
+        }
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    public void rightRotation(int[] nums,int rotation){
+        for (int i = 0; i < rotation; i++) {
+            int j, last = nums[nums.length-1];
+            for (j = nums.length-1; j >0; j--) {
+                nums[j] = nums[j-1];
+            }
+            nums[j] = last;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
 }
