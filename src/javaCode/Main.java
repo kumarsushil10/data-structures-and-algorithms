ddd3191744e25407856c6f159b9373e4d5a29c08
Sushil Kumar <kmrsushil10@gmail.com>
2023-12-02 10:02:02 +0530

    triangle matrix

--- src/javaCode/Main.java
+++ src/javaCode/Main.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         MatrixProblems matrixProblems = new MatrixProblems();
-        int[][] matrix1 = new int[][]{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
-        int[][] matrix2 = new int[][]{{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
-        System.out.println(matrixProblems.isBothMatrixSame(matrix1, matrix2));
+        int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        System.out.println("Lower Triangle Matrix");
+        matrixProblems.lowerTriangleMatrix(matrix);
+        System.out.println("Upper Triangle Matrix");
+        matrixProblems.upperTriangleMatrix(matrix);
     }
 }
--- src/javaCode/MatrixProblems.java
+++ src/javaCode/MatrixProblems.java
@@ -50,4 +50,30 @@ public class MatrixProblems {
         }
         return true;
     }
+
+    public void lowerTriangleMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i < j) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public void upperTriangleMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i > j) {
+                    System.out.print(0 + " ");
+                } else {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
