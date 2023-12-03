c9eb809468167a619364f61cf97f0f6e62e4846c
Sushil Kumar <kmrsushil10@gmail.com>
2023-12-03 13:52:39 +0530

    no of characters

--- src/javaCode/Main.java
+++ src/javaCode/Main.java
@@ -2,9 +2,10 @@ package javaCode;
 
 public class Main {
     public static void main(String[] args) {
-        MatrixProblems matrixProblems = new MatrixProblems();
-        int[][] matrix1 = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] matrix2 = new int[][]{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
-        System.out.println(matrixProblems.isSparseMatrix(matrix1));
+        StringProblems stringProblems = new StringProblems();
+        String str = "The best of both worlds";
+        System.out.println(str.length());
+        System.out.println(stringProblems.noOfCharacters(str));
+
     }
 }
